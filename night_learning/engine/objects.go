89e00a517037b52fae18307a27d33bb01359ab61
xieWei <wuxiaoshen@shu.edu.cn>
2018-05-17 00:53:01 +0800

    crawler with go

---
+++ night_learning/engine/objects.go
@@ -0,0 +1,23 @@
+package engine
+
+import "github.com/PuerkitoBio/goquery"
+
+type Request struct {
+	Url       string
+	ParseFunc func(*goquery.Document) Contents
+}
+
+type Content struct {
+	URL          string
+	Title        string
+	Tag          string
+	Reporter     string
+	ReporterHome string
+	Followers    int
+	Answers      int
+	Answerer     string
+	See          int
+	Passage      string
+}
+
+type Contents []Content

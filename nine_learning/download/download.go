75f3c500fa122bc02b1f5fbe8f1e22e52b950552
xieWei <wuxiaoshen@shu.edu.cn>
2018-05-21 23:25:00 +0800

    add other lesson

--- nine_learning/download/download.go
+++ nine_learning/download/download.go
@@ -5,6 +5,8 @@ import (
 
 	"errors"
 
+	"io/ioutil"
+
 	"github.com/PuerkitoBio/goquery"
 )
 
@@ -34,4 +36,27 @@ func Download(url string) (*goquery.Document, error) {
 	defer resp.Body.Close()
 
 	return goquery.NewDocumentFromReader(resp.Body)
+}
+
+func Downloader(url string) ([]byte, error) {
+	var (
+		req *http.Request
+		err error
+	)
+	if req, err = http.NewRequest("GET", url, nil); err != nil {
+		return nil, ErrorHttpRequest
+	}
+
+	client := http.DefaultClient
+	req.Header.Add("User-Agent", "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.139 Safari/537.36")
+	var (
+		resp *http.Response
+	)
+	if resp, err = client.Do(req); err != nil {
+		return nil, ErrorHttpResponse
+	}
+
+	defer resp.Body.Close()
+
+	return ioutil.ReadAll(resp.Body)
 }

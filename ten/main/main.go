a95a927a466a55ab9b9b703e1115ce00feb243cc
XieWei <wuxiaoshen@shu.edu.cn>
2018-05-31 21:42:42 +0800

    Update main.go

--- ten/main/main.go
+++ ten/main/main.go
@@ -6,4 +6,24 @@ func main() {
 	NewApp := app.NewConfigInstance("2")
 	NewApp.Instance("git@github.com:wuxiaoxiaoshen/Resume.git")
 
+
+	var dictMap = map[int32]string{}
+	dictMap[0x8c22] = "xi?
+	dictMap[0x4f1f] = "w?i"
+	var a = "??"
+	pys := [][]string{}
+	for _, v := range a {
+		py, ok := dictMap[v]
+		pyr := []string{}
+		if ok {
+			fmt.Println(py)
+			pyr = strings.Split(py, ",")
+		}
+		pys = append(pys, pyr)
+	}
+
+	fmt.Println(pys)
+
+
+
 }

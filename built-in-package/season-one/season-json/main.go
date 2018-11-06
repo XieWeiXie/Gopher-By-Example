f097b231bb65c8316193a9e8a5a11d21d5e27cbd
XieWei <wuxiaoshen@shu.edu.cn>
2018-11-06 17:13:06 +0800

    update json

--- built-in-package/season-one/season-json/main.go
+++ built-in-package/season-one/season-json/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"reflect"
 	"time"
 )
 
@@ -25,9 +26,10 @@ func main() {
 
 	var info Info
 	info = Info{
-		Name: "XieWei",
-		Age:  100,
-		City: "shangHai",
+		Name:    "XieWei",
+		Age:     100,
+		City:    "shangHai",
+		Company: "Only Me",
 	}
 	fmt.Println(string(info.MarshalOp()))
 
@@ -35,18 +37,18 @@ func main() {
 	otherInfo.Name = ""
 	otherInfo.Age = 20
 	otherInfo.City = "BeiJing"
+	otherInfo.Company = "Only You"
 	fmt.Println(string(otherInfo.MarshalOp()))
 
-	fmt.Println(UnMarshalExample([]byte(`{"name":"xiewei", "age": "20", "city_shanghai": "Guangdong"}`)))
+	fmt.Println(UnMarshalExample([]byte(`{"name":"xieWei", "age": "20", "city_shanghai": "GuangDong"}`)))
 
 	var self = SelfMarshal{}
 	self.Age = 20
 	self.Name = "XieWei"
 	self.City = "HangZhou"
-	fmt.Println(self, 12)
 
-	selfjsjjadjsajdjaj, err := json.Marshal(self)
-	fmt.Println(err, string(selfjsjjadjsajdjaj))
+	selfJsonMarshal, err := json.Marshal(self)
+	fmt.Println(err, string(selfJsonMarshal))
 
 	var t = Test{}
 	t.Date = jsonTime(time.Now())
@@ -54,6 +56,15 @@ func main() {
 	t.State = true
 	body, _ := json.Marshal(t)
 	fmt.Println(string(body))
+
+	//var name = make(map[string]interface{})
+	//name["key1"] = 123
+	//name["key2"] = "string key"
+	//name["key3"] = []string{"How", "Are", "You"}
+	//name["key4"] = info
+	var name = []byte(`{"key1":123,"key2":"string key", "key3":["How", "Are", "You"], "key4": {"name": "xieWei", "age":20,"city":"shanghai"}}`)
+
+	marshalInterface([]byte(name))
 
 }
 
@@ -75,9 +86,10 @@ func intToJson(value int) []byte {
 }
 
 type Info struct {
-	Name string `json:"name,omitempty"`
-	Age  int    `json:"age,string"`
-	City string `json:"city_shanghai"`
+	Name    string `json:"name,omitempty"`
+	Age     int    `json:"age,string"`
+	City    string `json:"city_shanghai"`
+	Company string `json:"-"`
 }
 
 func (i Info) MarshalOp() []byte {
@@ -98,9 +110,12 @@ type SelfMarshal struct {
 }
 
 func (self SelfMarshal) MarshalJSON() ([]byte, error) {
-	result := fmt.Sprintf("name:%s,age:%d,city:%s", self.Name, self.Age, self.City)
-	fmt.Println(result, 98765432)
-	return json.Marshal(result)
+	result := fmt.Sprintf("name:--%s,age:--%d,city:--%s", self.Name, self.Age, self.City)
+	if !json.Valid([]byte(result)) {
+		fmt.Println("invalid")
+		return json.Marshal(result)
+	}
+	return []byte(result), nil
 }
 
 type jsonTime time.Time
@@ -115,4 +130,23 @@ type Test struct {
 	Date  jsonTime `json:"date"`
 	Name  string   `json:"name"`
 	State bool     `json:"state"`
+}
+
+func marshalInterface(value []byte) {
+	var result map[string]interface{}
+	json.Unmarshal(value, &result)
+	fmt.Println(result)
+	for key, value := range result {
+		switch value.(type) {
+		case float64:
+			fmt.Println("Int", result[key], key, reflect.TypeOf(result[key]))
+		case string:
+			fmt.Println("String", result[key], key, reflect.TypeOf(result[key]))
+		case map[string]interface{}:
+			fmt.Println("Info", result[key], key, reflect.TypeOf(result[key]))
+		case []interface{}:
+			fmt.Println("[]string", result[key], key, reflect.TypeOf(result[key]))
+		}
+	}
+
 }
